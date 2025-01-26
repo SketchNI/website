271123e2201c4fb731aef2e7cb14d68ec853f48b
Sketch <sketch@sketchni.uk>
2025-01-26 02:53:59 +0000

    Maybe an audit log fix?
    Signed-off-by: Sketch <sketch@sketchni.uk>

--- resources/js/Pages/Backend/AuditLog/Index.vue
+++ resources/js/Pages/Backend/AuditLog/Index.vue
@@ -104,7 +104,7 @@ const page = usePage();
                                         <p class="text-text" v-text="log.causer.name" />
                                         <p class="text-subtext0 text-xs font-normal" v-text="log.causer.email" />
                                     </td>
-                                    <td class="w-2/12 pl-4 pr-3 py-2 text-sm font-medium">
+                                    <td v-if="log.subject_id !== null" class="w-2/12 pl-4 pr-3 py-2 text-sm font-medium">
                                         <p class="text-text" v-text="log.subject.name" />
                                         <p class="text-subtext0 text-xs font-normal" v-text="log.subject.email" />
                                     </td>
