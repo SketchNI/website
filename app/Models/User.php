3317ba880ecd56cd2265014461cffe4ab7536fab
Sketch <sketch@sketchni.uk>
2025-01-26 01:55:09 +0000

    Eepy mistake #3
    Signed-off-by: Sketch <sketch@sketchni.uk>

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,7 +78,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function setEmailVerified(bool $is_verified): self
     {
-        if ($this->isDirty('email_verified_at') && $is_verified) {
+        if ($this->email_verified_at !== null && $is_verified) {
             $this->email_verified_at = now();
         }
 
