b5558f9a3d5ca1dd629a4e43efff2eb890014919
Sketch <sketch@sketchni.uk>
2025-02-11 01:41:19 +0000

    Fix width of text input label

--- resources/js/Components/TextInputPrefix.vue
+++ resources/js/Components/TextInputPrefix.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="mt-1 border-t border-b border-l w-40 text-right py-2 px-2 border-overlay1 font-medium bg-base text-subtext0 shadow-sm shadow-surface1"
+        class="mt-1 border-t border-b border-l w-44 text-right py-2 px-2 border-overlay1 font-medium bg-base text-subtext0 shadow-sm shadow-surface1"
         v-text="value" />
 </template>
 <script setup>
