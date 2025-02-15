2226d39f66d1b4b079c059c67ca745d2e3c38ace
Sketch <sketch@sketchni.uk>
2025-02-15 22:58:35 +0000

    Assign role when a user signs up through github

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Traits\Flashable;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Laravel\Socialite\Facades\Socialite;
 use Symfony\Component\HttpFoundation\RedirectResponse as SymfonyRedirectResponse;
@@ -37,8 +39,11 @@ class LoginController extends Controller
             [
                 'name' => $socialite_user->getName(),
                 'email' => $socialite_user->getEmail(),
+                'email_verified_at' => now(),
             ]
         );
+
+        $user->assignRole('user');
 
         auth()->login($user->fresh());
 
