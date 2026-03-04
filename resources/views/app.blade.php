cf7dad1307c83e450184e43ca7ebe63796f7c92a
Sketch <sketch@sketchni.uk>
2026-03-04 17:14:47 +0000

    Add matomo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.bunny.net/css?family=anonymous+pro:400,700|sen:400,500,600,700,800|Nunito+Sans:wght@100..800&display=swap"
         crossorigin="anonymous"
         rel="stylesheet" />
-    <!-- Scripts -->
     @routes
     @vite(['resources/js/app.js'])
     @inertiaHead
@@ -20,10 +16,8 @@
     @stack('styles')
     @stack('scripts')
 
-    <!-- Matomo -->
     <script>
         var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
@@ -34,7 +28,6 @@
             g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
         })();
     </script>
-    <!-- End Matomo Code -->
 </head>
 <body class="font-sans antialiased h-full">
 @inertia
